8981a239d30041f71a22d8a96a83a85c84740e11
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-10-18 00:39:36 +0530

    .

--- netflix/src/Home/home.jsx
+++ netflix/src/Home/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./home.css";
 import Navbar from '../Components/Navbar/Navbar';
+import Featured from '../Components/featured/Featured';
 
 
 const home = () => {
   return (
     <div className='home'>
       <Navbar/>
+      <Featured type="movie"/>
     </div>
   )
 }
