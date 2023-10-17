bae3cbcfa0fd1c811dd5375dbb513c3172d73b34
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-10-17 18:46:32 +0530

    .

--- netflix/src/Home/home.jsx
+++ netflix/src/Home/home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./home.css";
+import Navbar from '../Components/Navbar/Navbar';
+
+
 
 const home = () => {
   return (
     <div className='home'>
+      <Navbar/>
     </div>
   )
 }
 
-export default home
+export default home;
